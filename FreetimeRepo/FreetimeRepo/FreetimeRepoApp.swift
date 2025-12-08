6a9d156c08892e87151fa87b216b60c87888752c
benchytrek <164025278+benchytrek@users.noreply.github.com>
2025-12-08 14:58:30 +0100

    xcode projekt erstellung

---
+++ FreetimeRepo/FreetimeRepo/FreetimeRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreetimeRepoApp.swift
+//  FreetimeRepo
+//
+//  Created by Ben Chytrek on 08.12.25.
+//
+
+import SwiftUI
+
+@main
+struct FreetimeRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
