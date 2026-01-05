61a2fef2d3c3a737ed108a04dc0f92c350c680ba
Anakin Skywalker (Ben Chytrek) <164025278+benchytrek@users.noreply.github.com>
2026-01-05 11:47:09 +0100

    CalenderInvite
    in die invites in calender anzeigen. nur funktion (verknüpfung) noch kein design formatierung

---
+++ FreetimeRepo/FreetimeRepo/View/Calender/CalendarInvite.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarInvite.swift
+//  FreetimeRepo
+//
+//  Created by Ben Chytrek on 05.01.26.
+//
+
+import SwiftUI
+
+struct CalendarInvite: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarInvite()
+}
