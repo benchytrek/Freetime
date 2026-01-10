68cfe97c5e0373367daa18ec9520a6615b9ca576
Anakin Skywalker (Ben Chytrek) <164025278+benchytrek@users.noreply.github.com>
2026-01-10 22:13:07 +0100

    New Goals
    bisschen viel mit gemini, der bruder muss nochmal ueberarbeitet werden

---
+++ FreetimeRepo/FreetimeRepo/View/Goals/GoalTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  GoalTaskView.swift
+//  FreetimeRepo
+//
+//  Created by Ben Chytrek on 10.01.26.
+//
+
+import SwiftUI
+
+struct GoalTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalTaskView()
+}
---
+++ FreetimeRepo/FreetimeRepo/View/Goals/GoalsDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  GoalsDetailView.swift
+//  FreetimeRepo
+//
+//  Created by Ben Chytrek on 10.01.26.
+//
+
+import SwiftUI
+
+struct GoalsDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalsDetailView()
+}
