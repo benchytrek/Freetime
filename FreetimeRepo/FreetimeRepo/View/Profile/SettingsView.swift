958e3b8f3d0afa0a964ecad02626270e90477871
Anakin Skywalker (Ben Chytrek) <164025278+benchytrek@users.noreply.github.com>
2026-01-11 22:11:25 +0100

    Profile/Settings Mockup
    ich muss in figma nochmal richtig ruebergehen und ueberlegen was da rein muss

---
+++ FreetimeRepo/FreetimeRepo/View/Profile/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  FreetimeRepo
+//
+//  Created by Ben Chytrek on 11.01.26.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
