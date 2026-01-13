a9ac7eae3ed57493a9e98db74a2e97befb6542b0
Anakin Skywalker (Ben Chytrek) <164025278+benchytrek@users.noreply.github.com>
2026-01-13 22:04:10 +0100

    Create Invite New
    work in progress, new pull down and title animation

---
+++ FreetimeRepo/FreetimeRepo/Test/PullToRevealTest.swift
@@ -0,0 +1,18 @@
+//
+//  PullToRevealTest.swift
+//  FreetimeRepo
+//
+//  Created by Ben Chytrek on 12.01.26.
+//
+
+import SwiftUI
+
+struct PullToRevealTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PullToRevealTest()
+}
---
+++ FreetimeRepo/FreetimeRepo/Test/ScrollViewTest.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewTest.swift
+//  FreetimeRepo
+//
+//  Created by Ben Chytrek on 12.01.26.
+//
+
+import SwiftUI
+
+struct ScrollViewTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollViewTest()
+}
